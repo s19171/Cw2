fa44e739048471e9b8d0e465659f83d350ebd89e
s19171 <61733699+s19171@users.noreply.github.com>
2020-03-10 10:20:15 +0100

    project

---
+++ Cw2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cw2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
